d28aac43b29c751295eb00e4e6b05c057c4b977f
Anna Scharl <anna.scharl@lifbi.de>
2020-10-28 13:13:03 +0100

    set up unit testing folders; adjust_school_context T/F now positive assignment

--- R/plausible_values.R
+++ R/plausible_values.R
@@ -279,17 +279,24 @@ plausible_values <- function(SC,
   type <- res[["type"]]
   waves <- res[["waves"]]
 
-  # school context only for SC3/4 while still in school
+  # school context only for SC2-4 while in school
   if (adjust_school_context) {
-      if (!(SC %in% c("SC3", "SC4"))) {
-          adjust_school_context <- FALSE
-      } else {
-          if (!longitudinal &&
-              (domain %in% c("MA", "RE") &
-              waves %in% c("_w9", "_w10"))) {
-              adjust_school_context <- FALSE
-          }
+    if (longitudinal) {
+      # some starting cohorts are never assessed in school context, thus, they
+      # can be excluded as a whole in the longitudinal case
+      if (SC %in% c("SC1", "SC5", "SC6")) {
+        adjust_school_context <- FALSE
       }
+    } else {
+      # some starting cohorts transition into or out of school, thus, specific
+      # waves have to be checked in the cross-sectional case
+      if (!(SC == "SC2" & wave %in% c("w3", "w4", "w5", "w6", "w9") ||
+            !(SC == "SC3" & wave %in% c("w1", "w2", "w3", "w5", "w6", "w7",
+                                        "w8", "w9")) ||
+            !(SC == "SC4" & wave %in% c("w1", "w2", "w3", "w5", "w7")))) {
+        adjust_school_context <- FALSE
+      }
+    }
   }
 
   # Begin data pre-processing -------------------------------------------------
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(NEPSscaling)
+
+test_check("NEPSscaling")
