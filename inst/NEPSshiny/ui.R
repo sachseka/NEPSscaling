c286381d959a49f724c25d53f6d00ec33dd09aa3
Anna Scharl <anna.scharl@lifbi.de>
2021-06-15 10:35:17 +0200

    updated -- contact info

--- inst/NEPSshiny/ui.R
+++ inst/NEPSshiny/ui.R
@@ -405,15 +405,15 @@ shinyUI(
           column(8,
                  HTML("If you have any questions or comments regarding NEPS<em>scaling</em>, please contact one of the following:"),
                  tags$ul(
-                   tags$li("The project Scaling and Test Design of the LIfBi, responsible for developing NEPSscaling: skalierung(at)lifbi.de"),
-                   tags$li("The NEPSforum: https://forum.lifbi.de/"),
-                   tags$li("The Research Data Center: fdz(at)lifbi.de")
+                   tags$li("The project", tags$a(href="mailto:skalierung(at)lifbi.de", tags$strong("Scaling and Test Design")), " of the LIfBi, responsible for developing ", HTML("NEPS<em>scaling</em>")),
+                   tags$li("The", tags$a(href="https://forum.lifbi.de/", tags$strong("NEPSforum")), " for general questions regarding the edition, dissemination and use of NEPS data"),
+                   tags$li("The", tags$a(href="https://www.neps-data.de/Data-Center/Contact-Data-Center", tags$strong("Research Data Center")), " for further information on the NEPS data")
                  )
          )
         )
       ),
       tabPanel(
-        "Background Information Plausible Values",
+        "Plausible Values",
         fluidRow(
           column(12,
                  tags$dl(
@@ -449,7 +449,7 @@ shinyUI(
         )
       ),
       tabPanel(
-        "Background Information CART",
+        "Classification and Regression Trees",
         fluidRow(
           column(12,
                  tags$dl(
