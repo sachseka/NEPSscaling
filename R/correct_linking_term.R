3e96a28d03ae049de16c8e795d3fa6cf4bf1ab92
Anna Scharl <anna.scharl@lifbi.de>
2021-10-21 15:11:37 +0200

    V2.3.0; deleted correction of linking term for SC3 science w8 and SC3 reading w9 [no longer necessary]

--- R/correct_linking_term.R
+++ R/correct_linking_term.R
@@ -21,15 +21,15 @@ correct_linking_term <- function(term, SC, domain, waves_, w) {
   } else if (SC == "SC3" & domain == "IC" & waves_[w] == "w5") {
     # means bias the wle estimate wrt the suf estimate
     term <- term + 0.2
-  } else if (SC == "SC3" & domain == "SC" & waves_[w] == "w8") {
-    # means bias the wle estimate wrt the suf estimate
-    term <- term - 0.1
+  #} else if (SC == "SC3" & domain == "SC" & waves_[w] == "w8") {
+  #  # means bias the wle estimate wrt the suf estimate
+  #  term <- term - 0.1
   } else if (SC == "SC3" & domain == "RE" & waves_[w] == "w6") {
     # means bias the wle estimate wrt the suf estimate
     term <- term + 0.1
-  } else if (SC == "SC3" & domain == "RE" & waves_[w] == "w9") {
-    # means bias the wle estimate wrt the suf estimate
-    term <- term - 0.1
+  #} else if (SC == "SC3" & domain == "RE" & waves_[w] == "w9") {
+  #  # means bias the wle estimate wrt the suf estimate
+  #  term <- term - 0.1
   } else if (SC == "SC4" & domain == "MA" & waves_[w] == "w7") {
     # longitudinal means bias the wle estimate wrt the suf estimate
     term <- term - 0.1
